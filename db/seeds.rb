bb244420cb05c234b2d7754ec1516713b216a5e5
asato.nakagawa <16h5092@stu.meisei-u.ac.jp>
2019-08-11 08:05:10 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(status: '完了でヤンす', content: 'スプラトゥーン')
+Task.create(status: 'まだでヤンす', content: '支払い')
+Task.create(status: 'まだでヤンす', content: '就職')
+(1..100).each do |number|
+  Task.create(status: '予定' + number.to_s, content: 'あ' + number.to_s)
+end
